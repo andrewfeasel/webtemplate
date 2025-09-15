4e7658a4feead0d6cd614ec1c0a0ec0fd5860710
Andrew <andrewmail404@gmail.com>
2025-09-14 19:43:01 -0700

    added server and routes (it is awesome guys)

---
+++ server.js
@@ -0,0 +1,32 @@
+import fs from "node:fs";
+import http from "node:http";
+
+var routes;
+fs.readFile("./routes.json", (err, data) => {
+  if(err) throw err;
+  routes = JSON.parse(data.toString("utf8"));
+});
+
+function serve(fileObj, res){
+  res.setHeader("Content-Type", fileObj.type);
+  res.writeHead(200);
+
+  fs.readFile(fileObj.path, (err, data) => {
+    if(err) throw err;
+    res.write(data);
+    res.end();
+  });
+}
+
+function serveStatic(req, res) {
+  if(Object.hasOwn(routes, req.url)) {
+    serve(routes[req.url], res);
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
+}
+
+var server = new http.Server();
+server.on("request", serveStatic);
+server.listen(8080);
